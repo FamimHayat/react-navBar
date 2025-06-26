5b83bedfc44edd65a8b24c4a8b2ea994d1d66e92
famim2002 <famimhayatyash@gmail.com>
2025-06-26 18:56:04 +0600

    react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import Layout from './layout/Index'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Demo from './pages/Demo'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route >
-                        
+          <Route path='/' element={<Layout/>} >
+            <Route index element={ <Home/>} />
+            <Route path='/' element={ <About/>} />
+            <Route path='/' element={ <Services/>} />
+            <Route path='/' element={ <Demo/>} />
+            <Route path='/' element={ <Contact/>} />
           </Route>
         </Routes>
       </BrowserRouter>
